ae523e76662e9aa9266c5368a7ac31b6ef5a78ce
monkeymoves <lukejjmaggs@gmail.com>
2018-10-20 01:34:13 +0100

    Whitford

--- myscript.js
+++ myscript.js
@@ -11,7 +11,7 @@ pannellum.viewer('panorama', {
     "scenes": {
         "CraigCerrigGleisiad": {
             "title": "Craig Cerrig Gleisiad",
-            "hfov": 100,
+            "hfov": 110,
             "pitch": -3,
             "yaw": 5,
             "type": "equirectangular",
@@ -29,7 +29,7 @@ pannellum.viewer('panorama', {
 
         "CraigCerrigGleisiad_upper": {
             "title": "Upper Section - Craig Cerrig Gleisiad",
-            "hfov": 100,
+            "hfov": 110,
             "yaw": -5,
             "type": "equirectangular",
             "panorama": "./img/CraigCerrigGleisiad_upper.jpg",
@@ -41,15 +41,15 @@ pannellum.viewer('panorama', {
                     "text": "Ogof Ffynnon Ddu",
                     "sceneId": "OgofFfynnonDdu",
                     "targetYaw": -23,
-                    "targetPitch": -20
+                    "targetPitch": -25
                 }
             ]
         },
     
     "OgofFfynnonDdu": {
             "title": "Ogof Ffynnon Ddu",
-            "hfov": 90,
-            "pitch": -75,
+            "hfov": 100,
+            "pitch": -6,
             "yaw": 117,
             "type": "equirectangular",
             "panorama": "./img/OgofFfynnonDdu.jpg",
@@ -58,22 +58,44 @@ pannellum.viewer('panorama', {
                     "pitch": -35.1,
                     "yaw": 15,
                     "type": "scene",
-                    "text": "Oxwich Bay National Nature Reserve",
-                    "sceneId": "OxwichBay"
+                    "text": "Whitford National Nature Reserve",
+                    "sceneId": "whitfordNNR"
+                }
+            ]
+        },
+
+        "whitfordNNR": {
+            "title": "Whitford National Nature Reserve",
+            "hfov": 120,
+            "yaw": 100,
+            "pitch": -20,
+            "horizonRoll": 20,
+
+            "type": "equirectangular",
+            "panorama": "./img/whitfordNNR.jpg", 
+            "hotSpots": [
+                {
+                    "pitch": -22,
+                    "yaw": 130,
+                    "type": "scene",
+                    "text": "OxwichBay National Nature Reserve",
+                    "sceneId": "OxwichBay",
+                    // "targetYaw": -50,
+                    // "targetPitch": 2
                 }
             ]
         },
 
         "OxwichBay": {
             "title": "OxwichBay National Nature Reserve",
-            "hfov": 90,
-            "yaw": 35,
+            "hfov": 120,
+            "yaw": 70,
             "type": "equirectangular",
             "panorama": "./img/OxwichBay.jpg",
             "hotSpots": [
                 {
                     "pitch": -0.6,
-                    "yaw": 100,
+                    "yaw": 90,
                     "type": "scene",
                     "text": "Return to Craig Cerrig Gleisiad",
                     "sceneId": "CraigCerrigGleisiad",
